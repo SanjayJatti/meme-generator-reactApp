ee5a818e48b3351cd3918a0496f575a1b56c17a2
Sanjay Jatti <88648908+SanjayJatti@users.noreply.github.com>
2021-12-19 06:45:38 -0800

    Docs: added memesData.js file and feat: added useState hook

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,13 +1,29 @@
+import React from "react";
+import memesData from "../memesData.js";
+
 export default function Meme() {
+
+ const [memeImg, setMemeImg] = React.useState("")
+
+ function getMemeImg(){
+   const memesArray = memesData.data.memes;
+   const randomNumber = Math.floor(Math.random() * memesArray.length)
+   setMemeImg(memesArray.[randomNumber].url)
+ } 
+
   return (
     <main>
-      <form className="meme--form">
+      <div className="meme--form">
         <div className="meme--input-div">
           <input className="meme--input" type="text" />
           <input className="meme--input" type="text" />
         </div>
-        <button className="meme--button">Get a new meme image</button>
-      </form>
+        <button 
+        className="meme--button"
+        onClick = {getMemeImg}
+        >Get a new meme image</button>
+      </div>
+      <img src ={memeImg} alt="" className="meme-image"/>
     </main>
   );
 }
